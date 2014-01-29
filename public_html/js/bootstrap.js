f416fdefb660d7b77eb048b7aca0f926f04a1169
deployer <marcosptf@yahoo.com.br>
2014-01-29 12:40:37 -0200

    preparando js min para quando foir subir em producao

--- public_html/js/bootstrap.js
+++ public_html/js/bootstrap.js
@@ -3,7 +3,6 @@
  * @since:19/09/2013
  * @desc:atribuição dos elementos html nas propriedades do jogo com YUI();
  ********************************************************************************/
-//    document.getElementById("divPrincipal").style.backgroundImage="file:///home/deployer/thewalkingdeadgame/public_html/img/bg/bg3.jpg";
 
 window.onload = function() {
     YUI().use("node", function(YGet) {
