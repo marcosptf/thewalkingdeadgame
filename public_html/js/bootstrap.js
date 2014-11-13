aa315b47c845c0be83f01f66c2ed65c351329bc7
marcosptf <marcosptf@yahoo.com.br>
2014-11-13 13:12:45 -0200

    Update bootstrap.js

--- public_html/js/bootstrap.js
+++ public_html/js/bootstrap.js
@@ -1,11 +1,16 @@
 /*******************************************************************************
  * @author:marcosptf@yahoo.com.br
  * @since:19/09/2013
- * @desc:atribuição dos elementos html nas propriedades do jogo com YUI();
+ * @desc:atribuicao dos elementos html nas propriedades do jogo com YUI();
  ********************************************************************************/
 
 window.onload = function() {
     YUI().use("node", function(YGet) {
         YGet.one("#divPrincipal").setStyle("backgroundImage", "url('"+BGImagePath+BGImage3+"')");
+		YGet.one("#divPrincipal").setStyle("background-repeat", "no-repeat");
+		YGet.one("#divPrincipal").setStyle("background-attachment", "fixed");
+		YGet.one("#divPrincipal").setStyle("height", "500px");
+		YGet.one("#divPrincipal").setStyle("width", "600px");
+		YGet.one("#divPrincipal").setStyle("top", "25px");		
     });
 }
