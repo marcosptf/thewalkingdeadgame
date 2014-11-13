508976a02c41856b0ae5220e967e035ca6fdebcb
marcosptf <marcosptf@yahoo.com.br>
2014-11-13 16:41:40 -0200

    Update bootstrap.js

--- public_html/js/bootstrap.js
+++ public_html/js/bootstrap.js
@@ -7,5 +7,10 @@
 window.onload = function() {
     YUI().use("node", function(YGet) {
         YGet.one("#divPrincipal").setStyle("backgroundImage", "url('"+BGImagePath+BGImage3+"')");
+		YGet.one("#divPrincipal").setStyle("background-repeat", "no-repeat");
+		YGet.one("#divPrincipal").setStyle("background-attachment", "fixed");
+		YGet.one("#divPrincipal").setStyle("height", "500px");
+		YGet.one("#divPrincipal").setStyle("width", "600px");
+		YGet.one("#divPrincipal").setStyle("top", "25px");		
     });
 }
